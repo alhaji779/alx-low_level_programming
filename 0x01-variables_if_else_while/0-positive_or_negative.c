fcc9a6b1394a30c25bbca00d179679012a031850
Michael Kanu <michaelkanu01@yahoo.com>
2023-08-11 01:38:13 -0700

    If else statement1

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * This is an if -else tutorial task
+ * n is a random variable
+ * output whether n is +ve, -ve or 0
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Studying if else while statements
