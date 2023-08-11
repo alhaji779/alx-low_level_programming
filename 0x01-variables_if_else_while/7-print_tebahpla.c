707d8414483fb39637aae0d3f1643808378e27ae
Michael Kanu <michaelkanu01@yahoo.com>
2023-08-11 10:21:02 -0700

    Reverse alpa

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always returns 0
+ *
+ */
+int main(void)
+{
+	char a = 'z';
+
+	for (; a < 'a'; a++)
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+}
