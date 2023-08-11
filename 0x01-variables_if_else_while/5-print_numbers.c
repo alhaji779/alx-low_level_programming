e367487161808ef53163d9e430081d7f85a7e986
Michael Kanu <michaelkanu01@yahoo.com>
2023-08-11 09:22:06 -0700

    Print digits only

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Program to display numbers
+ *
+ * return: always 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (; a < 10; a++;)
+	{
+		printf(a);
+	}
+
+	printf('\n');
+
+	return (0);
+}
