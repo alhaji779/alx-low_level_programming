c3b66ee89ab69559f9717c9b2b781e40cc7f0aec
Michael Kanu <michaelkanu01@yahoo.com>
2023-08-11 05:20:02 -0700

    List alphabeth

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Program to output the alphabets in lowercase
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char a;
+	for(a = 'a'; a < 'z' ; a++)
+	{
+		putchar(a);
+		putchar("\n");
+	}
+
+	return (0);
+}
