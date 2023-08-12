30c3e4a09df5f2f0444b58845d9c843ce96f3dc5
Michael Kanu <michaelkanu01@yahoo.com>
2023-08-12 08:40:01 -0700

    100-print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,17 +10,18 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = 10; a <= 19; a++)
+	for (a = 0; a < 9; a++)
 	{
-		for (b = 10; b <= 19; b++)
+		for (b = a + 1; b < 10; b++)
 		{
 			putchar((a % 10) + '0');
 			putchar((b % 10) + '0');
-			if (a != 19 || b != 19)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			if (a == 8 && b == 9)
+				continue;
+			
+			putchar(',');
+			putchar(' ');
+			
 		}
 	}
 	putchar('\n');
