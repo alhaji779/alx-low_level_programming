afc841bf4b9b6975bc7cfca2b41d2af94f0d5a20
Michael Kanu <michaelkanu01@yahoo.com>
2023-08-11 17:57:49 -0700

    Print combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always returns 0
+ *
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 10; a <= 19; a++)
+	{
+		for (b = 10; b = 19; b++)
+		{
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+			if (a != 19 || b != 19)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
