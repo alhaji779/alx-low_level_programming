0c81fee208dd09c4eb4cad0a5632e750dc9f76ee
Michael Kanu <michaelkanu01@yahoo.com>
2023-08-11 17:37:27 -0700

    Print with commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always equals 0
+ *
+ */
+int main(void)
+{
+	int a = '0';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
