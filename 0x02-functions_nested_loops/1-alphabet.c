4011c7d45b1c12e5a8086cc09118eb39e1fcb3e4
Michael Kanu <michaelkanu01@yahoo.com>
2023-08-15 16:02:24 -0700

    Prototype alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - contains actual code
+ *
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar('a');
+	}
+	_putchar('\n');
+
+	return (0);
+}
