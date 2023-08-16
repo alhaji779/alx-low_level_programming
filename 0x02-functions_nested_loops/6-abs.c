3a0bed194d8ccf9c0e56382311a8848750e52a53
Michael Kanu <michaelkanu01@yahoo.com>
2023-08-15 17:27:41 -0700

    get absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _abs - converts to absolute value
+ *
+ * @int: - requires an integer arg
+ *
+ * Return: absolute value
+ *
+ */
+int _abs(int)
+{
+	int c;
+
+	if (c > 0)
+	{
+		return (c);
+	}
+	else
+	{
+		return (c * -1);
+	}
+}
