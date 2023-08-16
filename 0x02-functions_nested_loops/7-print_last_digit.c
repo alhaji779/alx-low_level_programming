580bd7650dc2970091cd4312ca39a130742ec1ac
Michael Kanu <michaelkanu01@yahoo.com>
2023-08-15 17:39:17 -0700

    Print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ *
+ * @c: required int
+ *
+ * Return: last digit
+ */
+int print_last_digit(int c)
+{
+	int x = c % 10;
+
+	return (x);
+}
