62d01288effcae72d29a473c7f11fc267a16b559
Michael Kanu <michaelkanu01@yahoo.com>
2023-08-15 18:36:39 -0700

    add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - function to add numbers
+ *
+ * @x: - first argument
+ * @y: - second argument
+ *
+ * Return: the sum of both args
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
