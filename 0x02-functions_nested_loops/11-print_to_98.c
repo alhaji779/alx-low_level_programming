3cf70ecae4f6a1d995204c7d859f8abce5789f53
Michael Kanu <michaelkanu01@yahoo.com>
2023-08-16 15:50:21 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_to_98 - prints natural numbers
+ *
+ * @n: argument
+ *
+ * Result: always 0
+ *
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		_putchar(i + ',');
+	}
+
+	_putchar('\n');
+}
