ba35d0cf73678b0b370c8131b74868a2c4a90cb8
Michael Kanu <michaelkanu01@yahoo.com>
2023-08-18 14:25:19 -0700

    more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers multiple times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int h;
+
+	for (h = 0; h <= 9; h++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
