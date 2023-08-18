59edd4dc8ed67055fd44ec136d4c4b352304401a
Michael Kanu <michaelkanu01@yahoo.com>
2023-08-18 15:09:36 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_square
+ *
+ * @size: args
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
