d8da20adb0d79dc10e6c627cb1cb729140e26374
Michael Kanu <michaelkanu01@yahoo.com>
2023-08-18 07:29:23 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - used to print numbers
+ *
+ * Return: always 0
+ */
+void print_numbers(void)
+{
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
