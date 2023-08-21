6692d3291ad4838becb4ae177acf69c335314b2f
Michael Kanu <michaelkanu01@yahoo.com>
2023-08-21 14:53:43 -0700

    convert str to int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _atoi - convert string to int
+ * @s: char input
+ *
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int res = 0;
+	bool is_negative = false;
+
+	if (*s == '-')
+	{
+		is_negative = true;
+		s++;
+	}
+
+	while (*s)
+	{
+		if (*s < '0' || *s > '9')
+		{
+			return (0);
+		}
+		res = res * 10 + (*s - '0');
+		s++;
+	}
+
+	if (is_negative)
+	{
+		res = -res;
+	}
+
+	return (res);
+}
