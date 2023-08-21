db120c069916e8907fc451adde6d25b8c6f155ec
Michael Kanu <michaelkanu01@yahoo.com>
2023-08-21 13:46:09 -0700

    print numbers in even order

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - prints characters staring with new lines
+ * @str: char args
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i < _strlen(str); i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
