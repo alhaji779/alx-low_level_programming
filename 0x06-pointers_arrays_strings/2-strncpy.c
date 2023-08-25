45e8709c4e93447f8746b7699ff0471566301333
Michael Kanu <michaelkanu01@yahoo.com>
2023-08-25 13:31:25 -0700

    cpoy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @dest: first args
+ * @src: second args
+ * @n: number to append
+ *
+ * Return: char always
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i:
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
