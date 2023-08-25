03744cff8be9eb102fdec7cbbfae39dadd48c5b9
Michael Kanu <michaelkanu01@yahoo.com>
2023-08-25 14:28:06 -0700

    Upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - convert string to upper
+ * @char: args
+ *
+ * Return: char
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+
+	return (s);
+}
