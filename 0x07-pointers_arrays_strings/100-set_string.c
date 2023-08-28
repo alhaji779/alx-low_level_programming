dd587706371850860c7ac6afeec22b22dd8ce990
Michael Kanu <michaelkanu01@yahoo.com>
2023-08-28 10:51:33 -0700

    change character

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - switch pointer to char
+ * @s: pointer to pointer
+ * @to: second pointer args
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
