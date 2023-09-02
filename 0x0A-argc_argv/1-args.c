599f4be6e6c23496f11d26ea602824f296ff7328
Michael Kanu <michaelkanu01@yahoo.com>
2023-09-02 15:13:20 -0700

    print args name

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#define UNUSED(x) (void)(x)
 /**
  * main - main function
- * @argc: argumentc
+ * @argc: argument count
  * @argv: vector of arguments
  *Return: always 0
  */
-int main(int argc, char  *argv[])
+int main(int argc, char *argv[])
 {
+	UNUSED(argv);
 	printf("%d\n", argc - 1);
 	return (0);
 }
