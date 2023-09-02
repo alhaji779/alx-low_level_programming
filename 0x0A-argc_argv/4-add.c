7ab958a5d8e87fbffe30643990f78851c7c0b79a
Michael Kanu <michaelkanu01@yahoo.com>
2023-09-02 16:10:17 -0700

    addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+/**
+ * is_positive - helper function
+ * @str: arg
+ * Return: True or False
+ */
+bool is_positive(const char *str)
+{
+	int i;
+
+    	for (i = 0; str[i] != '\0'; i++)
+    	{
+        	if (str[i] < '0' || str[i] > '9')
+		{
+            	return false;
+        	}
+    	}
+    	return true;
+}
+
 /**
  * main - main function
  * @argc: argument count
@@ -20,7 +40,15 @@ int main(int argc, char *argv[])
 	{
 		for (i = 0; i < argc; i++)
 		{
-			sum += atoi(argv[i]);
+			if (is_positive(argv[i]))
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 		printf("%d\n", sum);
 		return (0);
