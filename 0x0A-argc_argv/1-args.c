f0e133edaac1bac04fa08cc121b483785208ab5d
Michael Kanu <michaelkanu01@yahoo.com>
2023-09-02 15:07:44 -0700

    print args name

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+ */
+int main(int argc, char  *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
