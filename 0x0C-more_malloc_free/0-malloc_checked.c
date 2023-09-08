e75268bced4e80b218451cb7cd3e61281e07a92f
Michael Kanu <michaelkanu01@yahoo.com>
2023-09-08 07:10:35 -0700

    malloc space

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - checks malloc
+ * @b: integer to check?
+ * Return: a pointer or void
+ */
+void *malloc_checked(unsigned int b)
+{
+
+	void *a;
+
+	t = malloc(b);
+	if (t == NULL)
+		exit(98);
+
+	return (a);
+}
