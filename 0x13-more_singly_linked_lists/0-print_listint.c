8a0c78f4c028aa81e65fb7fb8941e0a449b509e8
Michael Kanu <michaelkanu01@yahoo.com>
2023-09-25 04:51:57 -0700

    print int list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_listint - funct to print int in list
+ * @h: struct listint_t type
+ * Return: size_t
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
