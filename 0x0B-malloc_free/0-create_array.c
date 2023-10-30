818f121dfaee64b0f193d5f7b99b21b107175c19
Michael Kanu <michaelokanu01@yahoo.com>
2023-10-30 12:32:56 -0700

    malloc1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,6 +12,7 @@
 char *create_array(unsigned int size, char c)
 {
     char *ptrArry;
+    unsigned int i;
 
     ptrArry = malloc(size * sizeof(char));
     if (size <= 0)
@@ -23,7 +24,7 @@ char *create_array(unsigned int size, char c)
     {
         return (NULL);
     }
-    unsigned int i;
+
     for (i=0; i < size; i++)
         ptrArry[i] = c;
 
