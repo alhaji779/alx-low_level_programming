9379bfe967bca3a623f1426002cd33390539ace9
Michael Kanu <michaelokanu01@yahoo.com>
2023-10-30 12:28:45 -0700

    malloc 1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+ * create_array - function to createptr array
+ * @size: size of array
+ * @c: char args
+ * Return: char pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+    char *ptrArry;
+
+    ptrArry = malloc(size * sizeof(char));
+    if (size <= 0)
+    {
+        return (NULL);
+    }
+
+    if (ptrArry == NULL)
+    {
+        return (NULL);
+    }
+    unsigned int i;
+    for (i=0; i < size; i++)
+        ptrArry[i] = c;
+
+
+    return (ptrArry);
+}
