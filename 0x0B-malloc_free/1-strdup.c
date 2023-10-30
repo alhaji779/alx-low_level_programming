447a538bcfce54e288b6771d158c550305f57a15
Michael Kanu <michaelokanu01@yahoo.com>
2023-10-30 12:47:18 -0700

    malloc2

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - funct to dup str
+ * @str: string to copy
+ * Return: char pointer
+ */
+char *_strdup(char *str)
+{
+	int i;
+	int cnt = 0;
+	char *ptrArry;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[cnt] != '\0')
+		cnt++;
+
+	t = malloc(sizeof(char) * cnt + 1);
+
+	if (ptrArry == NULL)
+		return (NULL);
+
+	for (i = 0; i < cnt; i++)
+		ptrArry[i] = str[i];
+
+	return (ptrArry);
+
+}
