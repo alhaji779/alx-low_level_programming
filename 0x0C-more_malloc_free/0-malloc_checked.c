34de95bafa948ed726b5e2df7bd06d0669b43b8e
Michael Kanu <michaelokanu01@yahoo.com>
2023-11-02 06:19:19 -0700

    check malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - checks malloc
+ * @b: int arg
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+
+	void *ptArry;
+
+	ptArry = malloc(sizeof(unsigned int));
+	if (ptArry == NULL)
+		exit(98);
+
+	return (ptArry);
+}
