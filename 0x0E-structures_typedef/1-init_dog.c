5e747095383ac60368894a592a9ac4798f4b767d
Michael Kanu <michaelokanu01@yahoo.com>
2023-11-06 14:51:12 -0800

    init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - dog function
+ * @d: struct
+ * @name: char args
+ * @age: age args
+ * @owner: dog owner
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner
+}
