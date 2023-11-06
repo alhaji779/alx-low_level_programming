1325c87564ec06b6da1a332e40cdc2b8d4a36e8f
Michael Kanu <michaelokanu01@yahoo.com>
2023-11-06 14:39:05 -0800

    structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 /**
- * struct - dog
+ * struct dog - struct for a dog
  * @name: 1st args
  * @age: 2nd args
  * @owner: 3rd args
@@ -16,7 +16,5 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/*dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d); */
 
 #endif
