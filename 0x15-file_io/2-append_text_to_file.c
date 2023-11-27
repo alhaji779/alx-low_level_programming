8ec598f9e609f401a5ae8b9c505cd07b176a39c8
Michael Kanu <michaelokanu01@yahoo.com>
2023-11-27 14:15:21 -0800

    append_file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * append_text_to_file - appendfile
+ * @filename: filename
+ * @text_content: text
+ * Return: int 
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fn;
+	int len = 0;
+	long int result;
+
+	if (filename == NULL)
+		return (-1);
+	fn = open(filename, O_RDWR | O_APPEND);
+	if (fn == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+
+		result = write(fd, text_content, len);
+		if (result == -1)
+			return (-1);
+	}
+	if (close(fd) == -1)
+		return (-1);
+	return (1);
+}
